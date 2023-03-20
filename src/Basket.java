1676c874c46a5bc44a3b98396b993e70bcee82e8
Anastasiya <lazycat73332@gmail.com>
2023-03-20 20:08:11 +0300

    Выполнено задание 1

---
+++ src/Basket.java
@@ -0,0 +1,78 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class Basket {
+    private String[] productName;
+    private float[] productPrice;
+    private int[] productAmount;
+
+    public Basket() {
+
+    }
+
+    public Basket(String[] productName, float[] productPrice) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productAmount = new int[productName.length];
+    }
+
+    public Basket(String[] productName, float[] productPrice, int[] productAmount) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productAmount = productAmount;
+    }
+
+    public boolean addToCart(int productNumber, int amount) {
+        if ((productNumber >= 0) && (amount > 0)) {
+            for (int i = 0; i < productAmount.length; i++) {
+                if (i == productNumber) {
+                    productAmount[i] += amount;
+                    System.out.println("Товар добавлен в корзину");
+                    return true;
+                }
+            }
+        }
+        System.out.println("Не удалось добавить товар");
+        return false;
+    }
+
+    public void printCard() {
+        for (int i = 0; i < productName.length; i++) {
+            System.out.printf("\n%d. %15s (%7f руб/шт) %5d шт - %10f руб", i + 1, productName[i], productPrice[i], productAmount[i], productPrice[i] * productAmount[i]);
+        }
+    }
+
+    public boolean saveTxt(File textFile) throws IOException {
+        try (PrintWriter printWriter = new PrintWriter(textFile)) {
+            printWriter.println(productName.length);
+            for (int i = 0; i < productName.length; i++) {
+                printWriter.println(productName[i] + "\t" + productPrice[i] + "\t" + productAmount[i] + "\t");
+            }
+            return true;
+        }
+    }
+
+    public static Basket loadFromTxtFile(File textFile) throws IOException {
+        try (Scanner scanner = new Scanner(textFile)) {
+            int size = Integer.parseInt(scanner.nextLine());
+            String[] productName = new String[size];
+            int[] productAmount = new int[size];
+            float[] productPrice = new float[size];
+            for (int i = 0; i < size; i++) {
+                String[] s = scanner.nextLine().split("\t");
+                productName[i] = s[0];
+                productPrice[i] = Float.parseFloat(s[1]);
+                productAmount[i] = Integer.parseInt(s[2]);
+            }
+
+            Basket basket = new Basket(productName, productPrice, productAmount);
+            return basket;
+
+        }
+        //return null;
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String[] productNames = {"Хлеб", "Яблоки", "Молоко"};
+        float[] productPrices = {100f, 200f, 300f};
+        final String PATH = "D:\\Другое\\Проекты\\java_core_3\\файлы";
+        File dir = new File(PATH);
+        File textFile = new File(dir, "file2.txt");
+        String com;
+        boolean mark = true;
+        Basket basket = new Basket();
+        basket = new Basket(productNames, productPrices);
+        while (mark) {
+            try{
+                basket=Basket.loadFromTxtFile(textFile);}
+            catch (IOException ex) {
+                System.out.println("Не удалось найти файл");
+            }
+            System.out.println("\nВыберите номер команды или нажмите end:" +
+                    "\n1. Добавить товар в корзину" +
+                    "\n2. Проверить корзину товаров" +
+                    "\n3. Сохранить текущую корзину покупок" +
+                    "\n4. Восстановить корзину по названию файла\n");
+            Scanner scanner = new Scanner(System.in);
+            com = scanner.nextLine();
+            switch (com) {
+                case ("1") -> {
+                    Scanner scan = new Scanner(System.in);
+                    System.out.println("Введите номер товара и количество через пробел");
+                    basket.printCard();
+                    System.out.println("");
+                    String s=scan.nextLine();
+                    String[] parts = s.split(" ");
+                    try {
+                        int productNum = Integer.parseInt(parts[0]) - 1;
+                        int prodAmount = Integer.parseInt(parts[1]);
+                        basket.addToCart(productNum, prodAmount);
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
+                    break;
+                }
+                case ("2") -> {
+                    System.out.println("Ваша корзина:");
+                    basket.printCard();
+                    break;
+                }
+                case ("3") -> {
+                    if (!basket.equals(null)) {
+                        basket.saveTxt(textFile);
+                        System.out.println("Корзина сохранена");
+                    } else System.out.println("Корзина не сохранена");
+                    break;
+                }
+                case ("4") -> {
+                    Scanner scan = new Scanner(System.in);
+                    try{
+                    textFile = new File(dir, scan.nextLine());
+                    System.out.println("Введите название файла");
+                    basket=Basket.loadFromTxtFile(textFile);}
+                    catch (IOException ex) {
+                        System.out.println("Не удалось найти файл");
+                    }
+                    break;
+                }
+                case ("end") -> {
+                    mark = false;
+                    break;
+                }
+                default -> {
+                    System.out.println("Введенной команды не существует");
+                    break;
+                }
+            }
+
+
+        }
+
+    }
+}
